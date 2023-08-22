52ead07f0344f31b38b54727c3535c70ce8413af
AKSHAY KUMAR SINGH <akshaymemail@gmail.com>
2023-08-22 09:05:11 +0530

    upgraded to react 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -9,13 +9,19 @@ import 'react-toastify/dist/ReactToastify.css'
 
 const App = lazy(() => import('./App'))
 
-ReactDOM.render(
+// create container where your app will be loaded
+const container = document.getElementById('root')
+
+// create root
+const root = createRoot(container)
+
+// render your app
+root.render(
   <Provider store={store}>
     <Suspense fallback={<p>...</p>}>
       <App />
     </Suspense>
   </Provider>,
-  document.getElementById('root'),
 )
 
 // If you want to start measuring performance in your app, pass a function
