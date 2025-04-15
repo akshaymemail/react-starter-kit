f1ce21f1ff9ff7709c797c45e27bcf735dea15b0
AKSHAY KUMAR SINGH <akshaymemail@gmail.com>
2025-04-15 19:20:53 +0530

    fix: now localstorage and redux storage will be cleaned once logout action dispatched

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,9 +2,32 @@ import { combineReducers } from "redux";
 import authSlice from "./features/auth/slice";
 import todoSlice from "./features/todo/todoSlice";
 
-const reducers = combineReducers({
-  auth: authSlice.reducer,
-  todo: todoSlice.reducer,
-});
+// Define slices object once
+const slices = {
+  auth: authSlice,
+  todo: todoSlice,
+};
 
-export default reducers;
+// Create reducers object from slices
+const reducers = Object.fromEntries(
+  Object.entries(slices).map(([key, slice]) => [key, slice.reducer])
+);
+
+// Get initial state from slices
+const getInitialState = () =>
+  Object.fromEntries(
+    Object.entries(slices).map(([key, slice]) => [key, slice.getInitialState()])
+  );
+
+const appReducer = combineReducers(reducers);
+
+const rootReducer = (state, action) => {
+  if (action.type === slices.auth.actions.logout.type) {
+    // if logout action dispatched cleanup redux and localstorage
+    localStorage.clear();
+    return getInitialState();
+  }
+  return appReducer(state, action);
+};
+
+export default rootReducer;
